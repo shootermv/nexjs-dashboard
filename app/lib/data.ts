49b9ae5d72f286142c2122798411a87a9fcaf245
Moshe <shootermv@gmail.com>
2024-09-09 09:43:21 +0300

    feat: use route params at invoices page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,10 +15,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
