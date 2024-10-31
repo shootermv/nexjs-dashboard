95c0f1b298fbe300bb9ecf4babe9d59316d2332d
משה וילנר <moshe_v@malam-gemel.com>
2024-10-31 02:56:28 +0200

    fix(lint): remove unused import

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,6 @@
 import { fetchFilteredCustomers } from "@/app/lib/data";
 import Table from "@/app/ui/customers/table";
-import { lusitana } from "@/app/ui/fonts";
+
 export default async function Page() {
   const customers = await fetchFilteredCustomers("");
 
