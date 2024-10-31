b9713a23ac02d90aa08a72ede6812463de6d0aba
משה וילנר <moshe_v@malam-gemel.com>
2024-10-31 02:44:46 +0200

    feat: add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import Table from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+export default async function Page() {
+  const customers = await fetchFilteredCustomers("");
+
+  return (
+    <div className="w-full">
+      <Table customers={customers} />
+    </div>
+  );
 }
