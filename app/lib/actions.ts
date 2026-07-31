ee7b55b1b6c296b0053d83efcc66ff591ec7d844
moshe vilner <shootermv@gmail.com>
2026-07-31 14:33:43 +0300

    patch: use error syntax at zod validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -37,13 +37,13 @@ export async function authenticate(
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    invalid_type_error: "Please select a customer.",
+    error: "Please select a customer.",
   }),
   amount: z.coerce
     .number()
     .gt(0, { message: "Please enter an amount greater than $0." }),
   status: z.enum(["pending", "paid"], {
-    invalid_type_error: "Please select an invoice status.",
+    error: "Please select an invoice status.",
   }),
   date: z.string(),
 });
